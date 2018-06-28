21d26b4407b18a4d32e4cdea41520e87b8359eee
luisramos0 <luisramos0@gmail.com>
2018-06-28 01:52:09 +0100

    Added new cookies banner (with link to cookie policy page and the accept cookies button)

---
+++ app/controllers/api/cookies_consent_controller.rb
@@ -0,0 +1,25 @@
+module Api
+  class CookiesConsentController < BaseController
+    respond_to :json
+
+    def show
+      render json: { cookies_consent: cookies_consent.exists? }
+    end
+
+    def create
+      cookies_consent.set
+      show
+    end
+
+    def destroy
+      cookies_consent.destroy
+      show
+    end
+
+    private
+
+    def cookies_consent
+      @cookies_consent ||= CookiesConsent.new(cookies, request.host)
+    end
+  end
+end
